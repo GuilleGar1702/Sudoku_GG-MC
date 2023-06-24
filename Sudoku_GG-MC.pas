0367d403331ab9d7c98fbc345b07bb78f6f58376
Sak1803 <Sak180302@gmail.com>
2023-06-24 00:14:50 -0400

    Se cambiaron de lugar ciertos procedimientos y algunas variables se declararon como globables para mayor optimización

--- Sudoku_GG-MC.pas
+++ Sudoku_GG-MC.pas
@@ -15,6 +15,9 @@ var
     OpcionColumna, OpcionFila:integer;
     NumeroCasilla:char;
 
+	//--Variable para Matriz 1s--/
+    Matriz1S:Tmatriz1S;
+
 
 label
     EtiquetaImpresion1, EtiquetaMatriz1, EtiquetaComparador1;
@@ -110,13 +113,169 @@ begin
 		end;
 end;
 
-
-
-
-procedure CrearMatriz1S;
+procedure MostrarMatriz1;
 var Ancho,Alto:integer;
-    Matriz1S:Tmatriz1S;
+	Matriz1:TMatriz1;
 begin
+	for Ancho:=1 to TFila do
+		begin
+			if (Ancho=1) or (Ancho=2) or (Ancho=5) or (Ancho=8) or (Ancho=11) then
+				writeln('+---+-------+-------+-------+');
+			for Alto:=1 to TColumna do
+				begin
+					if (Alto=1) or (Alto=2) or (Alto=5) or (Alto=8) or (Alto=11) then
+						write('| ');
+						write(Matriz1[Ancho, Alto], ' '); 
+				end;
+	writeln;
+	end;
+end;
+
+
+
+procedure MostrarMatriz1P;
+var Ancho,Alto:integer;
+	Matriz1P:TMatriz1P;
+begin
+	for Ancho:=1 to TFila do
+		begin
+			if (Ancho=1) or (Ancho=2) or (Ancho=5) or (Ancho=8) or (Ancho=11) then
+				writeln('+---+-------+-------+-------+');
+			for Alto:=1 to TColumna do
+				begin
+					if (Alto=1) or (Alto=2) or (Alto=5) or (Alto=8) or (Alto=11) then
+						write('| ');
+						write(Matriz1P[Ancho, Alto], ' '); 
+				end;
+	writeln;
+	end;
+end;
+
+
+
+
+
+BEGIN
+    CrearMatriz1P;
+    MostrarMatriz1P;
+//--Area de creacion de la Matriz 1--//
+for Ancho:=1 to TFila do
+		begin
+			for Alto:=1 to TColumna do
+				begin
+					Matriz1[Ancho,Alto]:=(' ');
+                    Matriz1[1,2]:=('1');
+                    Matriz1[1,3]:=('2');
+                    Matriz1[1,4]:=('3');
+                    Matriz1[1,5]:=('4');
+                    Matriz1[1,6]:=('5');
+                    Matriz1[1,7]:=('6');
+                    Matriz1[1,8]:=('7');
+                    Matriz1[1,9]:=('8');
+                    Matriz1[1,10]:=('9');
+                    Matriz1[2,1]:=('1');
+                    Matriz1[3,1]:=('2');
+                    Matriz1[4,1]:=('3');
+                    Matriz1[5,1]:=('4');
+                    Matriz1[6,1]:=('5');
+                    Matriz1[7,1]:=('6');
+                    Matriz1[8,1]:=('7');
+                    Matriz1[9,1]:=('8');
+                    Matriz1[10,1]:=('9'); 
+                    //-----------------//
+                    Matriz1[2,3]:=('7');
+                    Matriz1[2,4]:=('9');
+                    Matriz1[2,6]:=('8');
+                    Matriz1[2,7]:=('2');
+                    Matriz1[2,8]:=('6');
+                    Matriz1[2,9]:=('4');
+                    Matriz1[3,4]:=('6');
+                    Matriz1[3,5]:=('7');
+                    Matriz1[3,6]:=('3');
+                    Matriz1[3,7]:=('9');
+                    Matriz1[3,8]:=('2');
+                    Matriz1[3,10]:=('8');
+                    Matriz1[4,3]:=('2');
+                    Matriz1[4,6]:=('5');
+                    Matriz1[4,7]:=('4');
+                    Matriz1[4,8]:=('3');
+                    Matriz1[4,9]:=('9');
+                    Matriz1[4,10]:=('7');
+                    Matriz1[5,2]:=('9');
+                    Matriz1[5,4]:=('5');
+                    Matriz1[5,5]:=('2');
+                    Matriz1[5,6]:=('1');
+                    Matriz1[5,7]:=('3');
+                    Matriz1[5,8]:=('8');
+                    Matriz1[5,10]:=('4');
+                    Matriz1[6,2]:=('7');
+                    Matriz1[6,3]:=('8');
+                    Matriz1[6,4]:=('3');
+                    Matriz1[6,7]:=('6');
+                    Matriz1[6,9]:=('5');
+                    Matriz1[6,10]:=('2');
+                    Matriz1[7,3]:=('4');
+                    Matriz1[7,4]:=('1');
+                    Matriz1[7,5]:=('5');
+                    Matriz1[7,6]:=('7');
+                    Matriz1[7,8]:=('9');
+                    Matriz1[7,9]:=('6');
+                    Matriz1[7,10]:=('3');
+                    Matriz1[8,2]:=('6');
+                    Matriz1[8,3]:=('1');
+                    Matriz1[8,6]:=('4');
+                    Matriz1[8,7]:=('5');
+                    Matriz1[8,9]:=('3');
+                    Matriz1[8,10]:=('9');
+                    Matriz1[9,2]:=('8');
+                    Matriz1[9,3]:=('3');
+                    Matriz1[9,5]:=('9');
+                    Matriz1[9,8]:=('5');
+                    Matriz1[9,9]:=('2');
+                    Matriz1[9,10]:=('1');
+                    Matriz1[10,3]:=('9');
+                    Matriz1[10,4]:=('7');
+                    Matriz1[10,5]:=('3');
+                    Matriz1[10,6]:=('2');
+                    Matriz1[10,7]:=('1');
+                    //-----------------//
+                    repeat
+                    EtiquetaMatriz1:
+                    writeln('Elija la casilla a llenar');
+                    writeln('Escriba el numero de columna');
+                    readln(OpcionColumna);
+                    writeln('Escriba el numero de fila');
+                    readln(OpcionFila);
+                    writeln('Escriba el numero que desea ingresar');
+                    readln(NumeroCasilla);
+                    Matriz1[OpcionFila+1,OpcionColumna+1]:=NumeroCasilla;
+                    goto EtiquetaImpresion1;
+                    until ((OpcionColumna>0) and (OpcionColumna<=9)) and ((OpcionFila>0) and (OpcionFila<=9)) 
+				end;
+		end;
+//--Fin del Area de creacion de la Matriz 1--//
+
+//--Area de impresion de Matriz 1--//
+EtiquetaImpresion1:
+    clrscr;
+    for Ancho:=1 to TFila do
+            begin
+                if (Ancho=1) or (Ancho=2) or (Ancho=5) or (Ancho=8) or (Ancho=11) then
+                    writeln('+---+-------+-------+-------+');
+                for Alto:=1 to TColumna do
+                    begin
+                        if (Alto=1) or (Alto=2) or (Alto=5) or (Alto=8) or (Alto=11) then
+                            write('| ');
+                            textcolor(lightred);
+                            write(Matriz1[Ancho, Alto], ' '); 
+                            textcolor(white);
+                    end;
+        writeln;
+        end;
+        goto EtiquetaMatriz1;
+//--Fin del Area de impresion de Matriz 1--//
+
+//--Area de Creación de la Matriz 1s--//
     for Ancho:=1 to TFila do
             begin
                 for Alto:=1 to TColumna do
@@ -223,53 +382,9 @@ begin
                         Matriz1S[10,10]:=('6');
                     end;
             end;
-        end;
+//--Fin de area de Creación Matriz 1s--//
 
-procedure MostrarMatriz1;
-var Ancho,Alto:integer;
-	Matriz1:TMatriz1;
-begin
-	for Ancho:=1 to TFila do
-		begin
-			if (Ancho=1) or (Ancho=2) or (Ancho=5) or (Ancho=8) or (Ancho=11) then
-				writeln('+---+-------+-------+-------+');
-			for Alto:=1 to TColumna do
-				begin
-					if (Alto=1) or (Alto=2) or (Alto=5) or (Alto=8) or (Alto=11) then
-						write('| ');
-						write(Matriz1[Ancho, Alto], ' '); 
-				end;
-	writeln;
-	end;
-end;
-
-
-
-procedure MostrarMatriz1P;
-var Ancho,Alto:integer;
-	Matriz1P:TMatriz1P;
-begin
-	for Ancho:=1 to TFila do
-		begin
-			if (Ancho=1) or (Ancho=2) or (Ancho=5) or (Ancho=8) or (Ancho=11) then
-				writeln('+---+-------+-------+-------+');
-			for Alto:=1 to TColumna do
-				begin
-					if (Alto=1) or (Alto=2) or (Alto=5) or (Alto=8) or (Alto=11) then
-						write('| ');
-						write(Matriz1P[Ancho, Alto], ' '); 
-				end;
-	writeln;
-	end;
-end;
-
-
-
-
-procedure MostrarMatriz1S;
-var Ancho,Alto:integer;
-	Matriz1S:TMatriz1S;
-begin
+//--Area de Impresión Matriz 1s--//
 	for Ancho:=1 to TFila do
 		begin
 			if (Ancho=1) or (Ancho=2) or (Ancho=5) or (Ancho=8) or (Ancho=11) then
@@ -282,129 +397,9 @@ begin
 				end;
 	writeln;
 	end;
-end;
-BEGIN
-    CrearMatriz1P;
-    MostrarMatriz1P;
-//--Area de creacion de la Matriz 1--//
-for Ancho:=1 to TFila do
-		begin
-			for Alto:=1 to TColumna do
-				begin
-					Matriz1[Ancho,Alto]:=(' ');
-                    Matriz1[1,2]:=('1');
-                    Matriz1[1,3]:=('2');
-                    Matriz1[1,4]:=('3');
-                    Matriz1[1,5]:=('4');
-                    Matriz1[1,6]:=('5');
-                    Matriz1[1,7]:=('6');
-                    Matriz1[1,8]:=('7');
-                    Matriz1[1,9]:=('8');
-                    Matriz1[1,10]:=('9');
-                    Matriz1[2,1]:=('1');
-                    Matriz1[3,1]:=('2');
-                    Matriz1[4,1]:=('3');
-                    Matriz1[5,1]:=('4');
-                    Matriz1[6,1]:=('5');
-                    Matriz1[7,1]:=('6');
-                    Matriz1[8,1]:=('7');
-                    Matriz1[9,1]:=('8');
-                    Matriz1[10,1]:=('9'); 
-                    //-----------------//
-                    Matriz1[2,3]:=('7');
-                    Matriz1[2,4]:=('9');
-                    Matriz1[2,6]:=('8');
-                    Matriz1[2,7]:=('2');
-                    Matriz1[2,8]:=('6');
-                    Matriz1[2,9]:=('4');
-                    Matriz1[3,4]:=('6');
-                    Matriz1[3,5]:=('7');
-                    Matriz1[3,6]:=('3');
-                    Matriz1[3,7]:=('9');
-                    Matriz1[3,8]:=('2');
-                    Matriz1[3,10]:=('8');
-                    Matriz1[4,3]:=('2');
-                    Matriz1[4,6]:=('5');
-                    Matriz1[4,7]:=('4');
-                    Matriz1[4,8]:=('3');
-                    Matriz1[4,9]:=('9');
-                    Matriz1[4,10]:=('7');
-                    Matriz1[5,2]:=('9');
-                    Matriz1[5,4]:=('5');
-                    Matriz1[5,5]:=('2');
-                    Matriz1[5,6]:=('1');
-                    Matriz1[5,7]:=('3');
-                    Matriz1[5,8]:=('8');
-                    Matriz1[5,10]:=('4');
-                    Matriz1[6,2]:=('7');
-                    Matriz1[6,3]:=('8');
-                    Matriz1[6,4]:=('3');
-                    Matriz1[6,7]:=('6');
-                    Matriz1[6,9]:=('5');
-                    Matriz1[6,10]:=('2');
-                    Matriz1[7,3]:=('4');
-                    Matriz1[7,4]:=('1');
-                    Matriz1[7,5]:=('5');
-                    Matriz1[7,6]:=('7');
-                    Matriz1[7,8]:=('9');
-                    Matriz1[7,9]:=('6');
-                    Matriz1[7,10]:=('3');
-                    Matriz1[8,2]:=('6');
-                    Matriz1[8,3]:=('1');
-                    Matriz1[8,6]:=('4');
-                    Matriz1[8,7]:=('5');
-                    Matriz1[8,9]:=('3');
-                    Matriz1[8,10]:=('9');
-                    Matriz1[9,2]:=('8');
-                    Matriz1[9,3]:=('3');
-                    Matriz1[9,5]:=('9');
-                    Matriz1[9,8]:=('5');
-                    Matriz1[9,9]:=('2');
-                    Matriz1[9,10]:=('1');
-                    Matriz1[10,3]:=('9');
-                    Matriz1[10,4]:=('7');
-                    Matriz1[10,5]:=('3');
-                    Matriz1[10,6]:=('2');
-                    Matriz1[10,7]:=('1');
-                    //-----------------//
-                    repeat
-                    EtiquetaMatriz1:
-                    writeln('Elija la casilla a llenar');
-                    writeln('Escriba el numero de columna');
-                    readln(OpcionColumna);
-                    writeln('Escriba el numero de fila');
-                    readln(OpcionFila);
-                    writeln('Escriba el numero que desea ingresar');
-                    readln(NumeroCasilla);
-                    Matriz1[OpcionFila+1,OpcionColumna+1]:=NumeroCasilla;
-                    goto EtiquetaImpresion1;
-                    until ((OpcionColumna>0) and (OpcionColumna<=9)) and ((OpcionFila>0) and (OpcionFila<=9)) and ((NumeroCasilla>0) and (NumeroCasilla<=9))
-				end;
-		end;
-//--Fin del Area de creacion de la Matriz 1--//
-//--Area de impresion de Matriz 1--//
-EtiquetaImpresion1:
-    clrscr;
-    for Ancho:=1 to TFila do
-            begin
-                if (Ancho=1) or (Ancho=2) or (Ancho=5) or (Ancho=8) or (Ancho=11) then
-                    writeln('+---+-------+-------+-------+');
-                for Alto:=1 to TColumna do
-                    begin
-                        if (Alto=1) or (Alto=2) or (Alto=5) or (Alto=8) or (Alto=11) then
-                            write('| ');
-                            textcolor(lightred);
-                            write(Matriz1[Ancho, Alto], ' '); 
-                            textcolor(white);
-                    end;
-        writeln;
-        end;
-        goto EtiquetaMatriz1;
-//--Fin del Area de impresion de Matriz 1--//
 
 //--Area de comparado--//
 EtiquetaComparador1:
-CrearMatriz1S;
 if Matriz1=Matriz1S then
     begin
         writeln('Felicidades, usted a completado el sudoku correctamente!!!');
@@ -421,9 +416,6 @@ else
 
 
 
-
-CrearMatriz1S;
-MostrarMatriz1S;
 readln;
 END.
 
