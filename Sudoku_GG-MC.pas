f773137a960ea8b3bfd51ec4f8210b855344e8d0
Guillermo <ggarcia.5065@unimar.edu.ve>
2023-06-25 18:03:21 -0400

    Este codigo se hizo en un ataque de preocupacion y motivación por la falta de tiempo, explicar todo lo que hice en un solo commit está dificil, así que te explico por whatsapp XD

--- Sudoku_GG-MC.pas
+++ Sudoku_GG-MC.pas
@@ -1,95 +1,298 @@
-program Sudoku;
+program Sudokum;
+
+uses
+  Crt;
 
 const
-  SIZE = 9; // Tamaño del tablero de Sudoku
+  Filas = 9;
+  Columnas = 9;
+  CuadranteTamano = 3;
+  PistasMinimas = 20;
 
 type
-  SudokuBoard = array[1..SIZE, 1..SIZE] of Integer;
+  Tablero = array[1..Filas, 1..Columnas] of Integer;
+  Pistas = array[1..Filas, 1..Columnas] of Boolean;
+  TableroR = array[1..Filas, 1..Columnas] of Integer;
+  PistasR = array[1..Filas, 1..Columnas] of Boolean;
 
-// Función para imprimir el tablero en pantalla
-procedure PrintBoard(board: SudokuBoard);
+var
+  Sudoku: Tablero;
+  PistasSudoku: Pistas;
+  SudokuR: TableroR;
+  PistasSudokuR: PistasR;
+
+procedure InicializarTablero(var Sudoku: Tablero);
 var
   i, j: Integer;
 begin
-  // Código para imprimir el tablero
+  for i := 1 to Filas do
+    for j := 1 to Columnas do
+      Sudoku[i, j] := 0;
 end;
 
-// Función para verificar si un número es válido en una posición del tablero
-function IsValidMove(board: SudokuBoard; row, col, num: Integer): Boolean;
-begin
-  // Código para verificar la validez del movimiento
-end;
+///////////////////////////////////////
 
-// Función para comprobar si el tablero está completamente resuelto
-function IsBoardFull(board: SudokuBoard): Boolean;
-begin
-  // Código para comprobar si el tablero está lleno
-end;
 
-// Función para cargar el tablero con las pistas iniciales
-procedure LoadPuzzle(var board: SudokuBoard);
-begin
-  // Código para cargar las pistas iniciales en el tablero
-end;
 
-// Función para resolver el Sudoku utilizando un algoritmo de backtracking
-function SolveSudoku(var board: SudokuBoard): Boolean;
-begin
-  // Código para resolver el Sudoku utilizando backtracking
-end;
+//////////////////////////////////////
 
-// Función principal del programa
+procedure MostrarTablero(Sudoku: Tablero; PistasSudoku: Pistas);
 var
-  board: SudokuBoard;
-  row, col, num: Integer;
+  i, j: Integer;
 begin
-  // Cargar el tablero con las pistas iniciales
-  LoadPuzzle(board);
-
-  // Mostrar el tablero inicial
-  PrintBoard(board);
-
-  // Solicitar los datos del usuario y permitirle jugar hasta que resuelva el Sudoku o se rinda
-  repeat
-    // Solicitar la posición y el número al usuario
-    writeln('Ingrese la fila:');
-    readln(row);
-    writeln('Ingrese la columna:');
-    readln(col);
-    writeln('Ingrese el número:');
-    readln(num);
-
-    // Verificar si el movimiento es válido
-    if IsValidMove(board, row, col, num) then
+  ClrScr;
+  
+  for i := 1 to Filas do
+  begin
+    for j := 1 to Columnas do
     begin
-      // Realizar el movimiento en el tablero
-      board[row, col] := num;
+      if PistasSudoku[i, j] then
+        TextColor(LightBlue)
+      else if Sudoku[i, j] = 0 then
+        TextColor(White)
+      else
+        TextColor(Yellow);
+      
+      write(Sudoku[i, j], ' ');
+      
+      if j mod CuadranteTamano = 0 then
+        write(' ');
+    end;
+    
+    writeln;
+    
+    if i mod CuadranteTamano = 0 then
+      writeln;
+  end;
+  
+  TextColor(White);
+end;
 
-      // Mostrar el tablero actualizado
-      PrintBoard(board);
-
-      // Verificar si el Sudoku está resuelto
-      if IsBoardFull(board) then
+function EsMovimientoValido(Sudoku: Tablero; fila, columna, numero: Integer): Boolean;
+var
+  i, j, inicioFila, inicioColumna: Integer;
+begin
+  for i := 1 to Columnas do
+  begin
+    if (Sudoku[fila, i] = numero) or (Sudoku[i, columna] = numero) then
+    begin
+      EsMovimientoValido := False;
+      Exit;
+    end;
+  end;
+  
+  inicioFila := CuadranteTamano * ((fila - 1) div CuadranteTamano) + 1;
+  inicioColumna := CuadranteTamano * ((columna - 1) div CuadranteTamano) + 1;
+  
+  for i := inicioFila to inicioFila + CuadranteTamano - 1 do
+    for j := inicioColumna to inicioColumna + CuadranteTamano - 1 do
+      if Sudoku[i, j] = numero then
       begin
-        writeln('¡Felicidades! Has resuelto el Sudoku.');
-        break;
+        EsMovimientoValido := False;
+        Exit;
       end;
+  
+  EsMovimientoValido := True;
+end;
+
+procedure RealizarMovimiento(var Sudoku: Tablero; fila, columna, numero: Integer);
+begin
+  Sudoku[fila, columna] := numero;
+end;
+
+
+//--Si el programa fuese un atomo, este procedimiento sería un neutrón, puro agregando peso, nada de carga util--//
+procedure EliminarMovimiento(var Sudoku: Tablero; fila, columna: Integer);
+begin
+  Sudoku[fila, columna] := 0;
+end;
+//--Volvemos con las cargas de verdad... (poronga de procedimiento)--//
+
+
+function TableroCompleto(Sudoku: Tablero): Boolean;
+var
+  i, j: Integer;
+begin
+  for i := 1 to Filas do
+    for j := 1 to Columnas do
+      if Sudoku[i, j] = 0 then
+      begin
+        TableroCompleto := False;
+        Exit;
+      end;
+  
+  TableroCompleto := True;
+end;
+
+function ResolverSudoku(var Sudoku: Tablero): Boolean;
+var
+  fila, columna, numero: Integer;
+begin
+  if TableroCompleto(Sudoku) then
+  begin
+    ResolverSudoku := True;
+    Exit;
+  end;
+  
+  for fila := 1 to Filas do
+    for columna := 1 to Columnas do
+      if Sudoku[fila, columna] = 0 then
+      begin
+        for numero := 1 to Columnas do
+        begin
+          if EsMovimientoValido(Sudoku, fila, columna, numero) then
+          begin
+            Sudoku[fila, columna] := numero;
+            
+            if ResolverSudoku(Sudoku) then
+            begin
+              ResolverSudoku := True;
+              Exit;
+            end;
+            
+            Sudoku[fila, columna] := 0;
+          end;
+        end;
+        
+        ResolverSudoku := False;
+        Exit;
+      end;
+  
+  ResolverSudoku := False;
+end;
+
+///////////////////////////////////////////////
+//--Acá estoy tratando de hacer que llene él mismo los espacios de las pistas--//
+
+function ResolverSudokuGenerado(var Sudoku: Tablero): Boolean;
+var
+  fila, columna, numero: Integer;
+begin
+  if TableroCompleto(Sudoku) then
+  begin
+    ResolverSudokuGenerado := True;
+    Exit;
+  end;
+  
+  for fila := 1 to Filas do
+    for columna := 1 to Columnas do
+      if Sudoku[fila, columna] = 0 then
+      begin
+        for numero := 1 to Columnas do
+        begin
+          if EsMovimientoValido(Sudoku, fila, columna, numero) then
+          begin
+            Sudoku[fila, columna] := numero;
+            
+            if ResolverSudokuGenerado(Sudoku) then
+            begin
+              ResolverSudokuGenerado := True;
+              Exit;
+            end;
+            
+            Sudoku[fila, columna] := 0;
+          end;
+        end;
+        
+        ResolverSudokuGenerado := False;
+        Exit;
+      end;
+  
+  ResolverSudokuGenerado := False;
+end;
+
+//-----------------------------------------------------------//
+
+procedure InicializarPistas(var PistasSudoku: Pistas);
+var
+  i, j, PistasGeneradas, fila, columna, numero: Integer;
+label
+  NumeroRandom;
+begin
+  PistasGeneradas := 0;
+  
+  for i := 1 to Filas do
+    for j := 1 to Columnas do
+      PistasSudoku[i, j] := False;
+      PistasSudokuR[i, j] := False;
+  
+  repeat
+    i := Random(Filas) + 1;
+    j := Random(Columnas) + 1;
+    fila:=i;
+    columna:=j;
+    NumeroRandom:
+    numero:=random(9)+1;
+    if not EsMovimientoValido(Sudoku, fila, columna, numero) then
+      begin
+        goto NumeroRandom;
+      end;
+    Sudoku[i, j]:=numero;
+    if not PistasSudoku[i, j] then
+    begin
+      PistasSudoku[i, j] := True;
+      PistasSudokuR[i, j] := True;
+      Inc(PistasGeneradas);
+    end;
+  until PistasGeneradas >= PistasMinimas;
+  if not ResolverSudokuGenerado(SudokuR) then
+    begin
+      goto NumeroRandom;
+    end;
+end;
+
+///////////////////////////////////////////////
+
+procedure JugarSudoku;
+var
+  fila, columna, numero: Integer;
+begin
+  Randomize;
+  writeln('Bienvenido a "TuSudoku C&G"');
+  InicializarTablero(Sudoku);
+  InicializarPistas(PistasSudoku);
+  
+  repeat
+    MostrarTablero(Sudoku, PistasSudoku);
+    
+    repeat
+      write('Ingrese la fila (1-9): ');
+      readln(fila);
+      write('Ingrese la columna (1-9): ');
+      readln(columna);
+    until (fila >= 1) and (fila <= Filas) and (columna >= 1) and (columna <= Columnas);
+    
+    if not PistasSudoku[fila, columna] then
+    begin
+      repeat
+        write('Ingrese el número (1-9): ');
+        readln(numero);
+      until (numero >= 1) and (numero <= Columnas);
+      
+      if EsMovimientoValido(Sudoku, fila, columna, numero) then
+        RealizarMovimiento(Sudoku, fila, columna, numero)
+      else
+        writeln('Movimiento inválido. Intente nuevamente.');
     end
     else
-    begin
-      writeln('Movimiento inválido. Por favor, intenta nuevamente.');
-    end;
-  until false;
-
-  // Comprobar si el usuario se rindió y mostrar la solución
-  if not IsBoardFull(board) then
+      writeln('No puede modificar una pista. Intente nuevamente.');
+    
+  until TableroCompleto(Sudoku);
+  
+  MostrarTablero(Sudoku, PistasSudoku);
+  
+  if ResolverSudoku(Sudoku) then
+    writeln('¡Felicidades! Has resuelto el Sudoku.')
+  else
   begin
-    writeln('Te has rendido. La solución es:');
-    SolveSudoku(board);
-    PrintBoard(board);
+    writeln('No se pudo resolver el Sudoku. La solución es:');
+    MostrarTablero(Sudoku, PistasSudoku);
   end;
-
-  // Esperar a que el usuario presione una tecla para salir
+  
   writeln('Presione cualquier tecla para salir.');
-  readln;
+  ReadKey;
+end;
+
+begin
+  JugarSudoku;
 end.
