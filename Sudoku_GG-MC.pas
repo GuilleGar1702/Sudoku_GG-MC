03ea00a17666302569146e875a63587206ba6cdc
Guillermo <ggarcia.5065@unimar.edu.ve>
2023-06-26 21:59:52 -0400

    Se quitó la opción de resolver un sudoku, se agregaron las instrucciones, se solucionó el bug de solucion de sudoku al rendirse

--- Sudoku_GG-MC.pas
+++ Sudoku_GG-MC.pas
@@ -13,11 +13,15 @@ type
   Pistas = array[1..Filas, 1..Columnas] of Boolean;
   TableroR = array[1..Filas, 1..Columnas] of Integer;
   PistasR = array[1..Filas, 1..Columnas] of Boolean;
+  TableroFinal = array[1..Filas, 1..Columnas] of Integer;
+  PistasFinal = array[1..Filas, 1..Columnas] of Boolean;
 var
   Sudoku: Tablero;
   PistasSudoku: Pistas;
   SudokuR: TableroR;
   PistasSudokuR: PistasR;
+  SudokuFinal: TableroFinal;
+  PistasSudokuFinal: PistasFinal;
 procedure InicializarTablero(var Sudoku: Tablero);
 var
   i, j: Integer;
@@ -26,6 +30,33 @@ begin
     for j := 1 to Columnas do
       Sudoku[i, j] := 0;
 end;
+
+function EsMovimientoValido(Sudoku: Tablero; fila, columna, numero: Integer): Boolean;
+var
+  i, j, inicioFila, inicioColumna: Integer;
+begin
+  for i := 1 to Columnas do
+  begin
+    if (Sudoku[fila, i] = numero) or (Sudoku[i, columna] = numero) then
+    begin
+      EsMovimientoValido := False;
+      Exit;
+    end;
+  end;
+  inicioFila := CuadranteTamano * ((fila - 1) div CuadranteTamano) + 1;
+  inicioColumna := CuadranteTamano * ((columna - 1) div CuadranteTamano) + 1;
+  for i := inicioFila to inicioFila + CuadranteTamano - 1 do
+    for j := inicioColumna to inicioColumna + CuadranteTamano - 1 do
+      if Sudoku[i, j] = numero then
+      begin
+        EsMovimientoValido := False;
+        Exit;
+      end;
+  EsMovimientoValido := True;
+end;
+
+//----------------------------------------------------------------------------------------//
+
 procedure MostrarTablero(Sudoku: Tablero; PistasSudoku: Pistas);
 var
   i, j: Integer;
@@ -51,38 +82,37 @@ begin
   end;
   TextColor(White);
 end;
-function EsMovimientoValido(Sudoku: Tablero; fila, columna, numero: Integer): Boolean;
+//----------------------------------------------------------------------------------------//
+
+
+//--Función nueva que aun no se utilizar bien xd--//
+
+procedure BorrarValoresIngresados(var Sudoku: Tablero; PistasSudoku: Pistas);
 var
-  i, j, inicioFila, inicioColumna: Integer;
+  i, j: Integer;
 begin
-  for i := 1 to Columnas do
-  begin
-    if (Sudoku[fila, i] = numero) or (Sudoku[i, columna] = numero) then
-    begin
-      EsMovimientoValido := False;
-      Exit;
-    end;
-  end;
-  inicioFila := CuadranteTamano * ((fila - 1) div CuadranteTamano) + 1;
-  inicioColumna := CuadranteTamano * ((columna - 1) div CuadranteTamano) + 1;
-  for i := inicioFila to inicioFila + CuadranteTamano - 1 do
-    for j := inicioColumna to inicioColumna + CuadranteTamano - 1 do
-      if Sudoku[i, j] = numero then
-      begin
-        EsMovimientoValido := False;
-        Exit;
-      end;
-  EsMovimientoValido := True;
+  for i := 1 to Filas do
+    for j := 1 to Columnas do
+      if not PistasSudoku[i, j] then
+        begin
+          Sudoku[i, j] := 0;
+        end;
 end;
+
+//--Fin de Función nueva que aun no se utilizar bien xd--//
+
 procedure RealizarMovimiento(var Sudoku: Tablero; fila, columna, numero: Integer);
 begin
   Sudoku[fila, columna] := numero;
 end;
+
 //--Si el programa fuese un atomo, este procedimiento sería un neutrón, puro agregando peso, nada de carga util, porque puedes cambiar el dato y ya, pero la comodidad mató al humano--//
 procedure EliminarMovimiento(var Sudoku: Tablero; fila, columna: Integer);
 var numero: shortint;
 begin
-  {Sudoku[fila, columna] := 0;}
+  {textcolor(white);
+  Sudoku[fila, columna] := 0;
+  numero:=0;}
   numero:=0;
   RealizarMovimiento(Sudoku, fila, columna, numero);
   MostrarTablero(Sudoku, PistasSudoku);
@@ -101,7 +131,7 @@ begin
       end;
   TableroCompleto := True;
 end;
-function ResolverSudoku(var Sudoku: Tablero): Boolean;
+function ResolverSudoku(var SudokuR: TableroR): Boolean;
 var
   fila, columna, numero: Integer;
 begin
@@ -133,31 +163,31 @@ begin
   ResolverSudoku := False;
 end;
 ///////////////////////////////////////////////
-function ResolverSudokuGenerado(var Sudoku: Tablero): Boolean;
+function ResolverSudokuGenerado(var SudokuR: TableroR): Boolean;
 var
   fila, columna, numero: Integer;
 begin
-  if TableroCompleto(Sudoku) then
+  if TableroCompleto(SudokuR) then
   begin
     ResolverSudokuGenerado := True;
     Exit;
   end;
   for fila := 1 to Filas do
     for columna := 1 to Columnas do
-      if Sudoku[fila, columna] = 0 then
+      if SudokuR[fila, columna] = 0 then
       begin
         for numero := 1 to Columnas do
         begin
-          if EsMovimientoValido(Sudoku, fila, columna, numero) then
+          if EsMovimientoValido(SudokuR, fila, columna, numero) then
           begin
-            Sudoku[fila, columna] := numero;
+            SudokuR[fila, columna] := numero;
             
-            if ResolverSudokuGenerado(Sudoku) then
+            if ResolverSudokuGenerado(SudokuR) then
             begin
               ResolverSudokuGenerado := True;
               Exit;
             end;
-            Sudoku[fila, columna] := 0;
+            SudokuR[fila, columna] := 0;
           end;
         end;
         ResolverSudokuGenerado := False;
@@ -166,6 +196,42 @@ begin
   ResolverSudokuGenerado := False;
 end;
 //-----------------------------------------------------------//
+function SudokuUnico(var Sudoku: Tablero): Boolean;
+var
+  CopiaSudoku: Tablero;
+begin
+  CopiaSudoku := Sudoku;
+  SudokuUnico := ResolverSudokuGenerado(Sudoku);
+end;
+//-------------------------------------------------------//
+procedure MostrarTableroFinal(SudokuFinal: TableroFinal; PistasSudokuFinal: PistasFinal);
+var
+  i, j: Integer;
+begin
+  ClrScr;
+  for i := 1 to Filas do
+  begin
+    for j := 1 to Columnas do
+    begin
+      if PistasSudoku[i, j] then
+        TextColor(LightBlue)
+      else if Sudoku[i, j] = 0 then
+        TextColor(White)
+      else
+        TextColor(Yellow);
+      write(Sudoku[i, j], ' ');
+      if j mod CuadranteTamano = 0 then
+        write(' ');
+    end;
+    writeln;
+    if i mod CuadranteTamano = 0 then
+      writeln;
+  end;
+  TextColor(White);
+end;
+
+
+//-------------------------------------------------------//
 procedure InicializarPistas(var PistasSudoku: Pistas);
 var
   i, j, PistasGeneradas, fila, columna, numero: Integer;
@@ -199,7 +265,7 @@ begin
       begin
         goto NumeroRandom;
       end;
-  until PistasGeneradas >= PistasDificil;
+  until (PistasGeneradas >= PistasDificil) and SudokuUnico(SudokuR);
   if not ResolverSudokuGenerado(SudokuR) then
       begin
         goto NumeroRandom;
@@ -210,39 +276,147 @@ var
   fila, columna, numero: Integer;
   Opcion1, Opcion2: shortint;
 label
-  EtiquetaInicio, EtiquetaSudoku, EtiquetaIngresar, EtiquetaRendirse;
+  EtiquetaInicio, EtiquetaSudoku, EtiquetaIngresar, EtiquetaRendirse, EtiquetaAccion;
 begin
   Randomize;
   EtiquetaInicio:
   textcolor(lightcyan);
+  repeat
+  clrscr;
   writeln('===============================================================================');
   writeln('|                          Bienvenido al "TuSudoku C&G"                       |');
   writeln('===============================================================================');
   Writeln('|                             Que accion desea tomar?                         |');
   writeln('===============================================================================');
-  writeln('|1-Generar Sudoku   2-Resolver Sudoku   3-Ensename a usar "TuSudoku"   4-Salir|');
+  writeln('|    1-Generar Sudoku         2-Ensename a usar "TuSudoku"             3-Salir|');
   writeln('===============================================================================');
   readln(Opcion1);
+  PistasSudokuFinal:=PistasSudokuR;
+  
   case Opcion1 of
     1:begin
         goto EtiquetaSudoku;
       end;
     2:begin
-        exit //--Aun no se hace la opcion--//
+		//--Inicio del area de Manual del Usuario--//
+		clrscr;
+					writeln('|=================================================================|');
+					writeln('|                      Bienvenido a TUSUDOKU                      |');
+					writeln('|                   A continuacion, recibira las                  |');
+					writeln('| Instrucciones para poder utilizar el programa de forma correcta |');
+					writeln('|=================================================================|');
+					writeln('   ');
+					writeln('Presione una tecla para continuar');
+					readln;
+					
+					writeln('|==============================|');
+					writeln('|      MANUAL DE USUARIO:      |');
+					writeln('|==============================|');
+					
+					writeln('   ');
+					
+					writeln('|==================================================|');
+					writeln('|1.Al entrar en el programa, tendra 3 opciones:    |');
+					writeln('|  A. Generar Sudoku                               |');
+					writeln('|  B. Ensename a usar TUSUDOKU                     |');
+					writeln('|  C. Salir                                        |');
+					writeln('|==================================================|');
+
+					writeln('   ');
+
+					writeln('|=================================================================|');
+					writeln('|2.Generar Sudoku:                                                |');
+					writeln('|  Esta opcion le permite generar un nuevo Sudoku para resolver.  |'); 
+					writeln('|  El programa generara un Sudoku aleatorio con una               |'); 
+					writeln('|  combinacion unica de pistas predefinidas.                      |'); 
+					writeln('|=================================================================|');
+
+					writeln('   ');
+					
+					writeln('Presione una tecla para continuar');
+					readln;
+
+
+					writeln('|=================================================================|');
+					writeln('|3. Ensename a usar TUSUDOKU:                                     |');
+					writeln('|  Esta seccion le proporcionara una guia detallada sobre como    |');
+					writeln('|  utilizar el programa TUSUDOKU. Aprendera como ingresar         |');
+					writeln('|  los valores en el tablero, como verificar la validez de su     |'); 
+					writeln('|  solucion y como usar las funciones adicionales disponibles.    |');
+					writeln('|=================================================================|');
+					
+					writeln('   ');
+					
+					writeln('|======================================|');
+					writeln('|4. Salir:                             |');
+					writeln('|   Utilice esta opcion para cerrar el |'); 
+					writeln('|   programa TUSUDOKU en su totalidad. |');
+					writeln('|======================================|');
+					
+					writeln('   ');
+					
+					writeln('A continuacion, se presentaran las instrucciones sobre como utilizar el apartado de'); 
+					writeln('''Utilizar Sudoku''');
+					writeln('   ');
+					
+					writeln('Presione una tecla para continuar');
+					readln;
+					
+					writeln('   ');
+					
+					writeln('A continuacion se presentan las instrucciones para rellenar el tablero:');
+					
+					writeln('');
+					writeln('|===================================================================================================================|');
+					writeln('|1. El tablero de Sudoku consta de una cuadricula de 9x9 casillas, dividida en                                      |');
+					writeln('|   9 subcuadriculas mas pequenas de 3x3.                                                                           |');
+					writeln('|                                                                                                                   |');
+					writeln('|2. Para rellenar el tablero, debe ingresar los valores del 1 al 9 en las casillas vacias.                          |');
+					writeln('|   Utilice la siguiente notacion:                                                                                  |');
+					writeln('|   - Ingrese el numero deseado en la casilla correspondiente.                                                      |');
+					writeln('|   - Si necesita borrar un valor ingresado previamente, utilice la opcion "Quitar valor".                          |');
+					writeln('|                                                                                                                   |');
+					writeln('|3. Puede comenzar a rellenar las casillas vacias de izquierda a derecha y de arriba a abajo, o                     |');
+					writeln('|   en el orden que prefiera                                                                                        |');
+					writeln('|                                                                                                                   |');
+					writeln('|4. Asegurese de cumplir con las siguientes reglas al ingresar los valores:                                         |');
+					writeln('|                                                                                                                   |');
+					writeln('|   - Cada fila debe contener todos los numeros del 1 al 9, sin repetir ninguno.                                    |');
+					writeln('|   - Cada columna debe contener todos los numeros del 1 al 9, sin repetir ninguno.                                 |');
+					writeln('|   - Cada subcuadricula de 3x3 debe contener todos los numeros del 1 al 9, sin repetir ninguno.                    |');
+					writeln('|                                                                                                                   |');
+					writeln('|5. Tenga en cuenta que el programa generara un Sudoku con una combinacion unica de pistas predefinidas.            |'); 
+					writeln('|No podra cambiar ni borrar estas pistas.                                                                           |');
+					writeln('|                                                                                                                   |');
+					writeln('|6. Una vez que haya ingresado todos los valores en el tablero, puede seleccionar la opcion "Resolver Sudoku"       |'); 
+					writeln('|para que el programa intente resolverlo y mostrar el resultado.                                                    |');
+					writeln('|                                                                                                                   |');
+					writeln('|===================================================================================================================|');
+					writeln('');
+					writeln('Disfrute resolviendo Sudokus con TUSUDOKU!');
+					writeln('');
+					writeln('Presione una tecla para continuar y regresar al menu principal');
+					readln;
+        //--Fin del Area del Manual del Usuario--//
       end;
     3:begin
-        exit //--Aun no se hace la opcion--//
+        exit 
       end;
-    4:begin
-      exit;
-    end;
+      
+      else
+      begin
+		writeln('La opcion ingresada no es valida');
+		end;
   end;
+  until opcion1=3;
   EtiquetaSudoku:
   InicializarTablero(Sudoku);
   InicializarPistas(PistasSudoku);
   repeat
     MostrarTablero(Sudoku, PistasSudoku);
     repeat
+      EtiquetaAccion:
+      textcolor(lightcyan);
       writeln('================================================================');
       Writeln('|                    Que accion desea tomar?                   |');
       writeln('================================================================');
@@ -266,6 +440,10 @@ begin
         4:begin
           exit;
         end;
+      else
+        begin
+          goto EtiquetaAccion;
+        end;
       end;
       EtiquetaIngresar:
       write('Ingrese la fila (1-9): ');
@@ -282,23 +460,31 @@ begin
       if EsMovimientoValido(Sudoku, fila, columna, numero) then
         RealizarMovimiento(Sudoku, fila, columna, numero)
       else
-        writeln('Movimiento inválido. Intente nuevamente.');
+        begin
+          writeln('Movimiento inválido. Intente nuevamente.');
+        end;
     end
     else
-      writeln('No puede modificar una pista. Intente nuevamente.');
+      begin
+        writeln('No puede modificar una pista. Intente nuevamente.');
+      end;
   until TableroCompleto(Sudoku);
   MostrarTablero(Sudoku, PistasSudoku);
   if ResolverSudoku(Sudoku) then
     begin
       writeln('¡Felicidades! Has resuelto el Sudoku.')
     end;
+    
+    
   EtiquetaRendirse:
   writeln('No se pudo resolver el Sudoku. La solución es:');
+  BorrarValoresIngresados(Sudoku, PistasSudoku);
   ResolverSudoku(Sudoku);
-  MostrarTablero(Sudoku, PistasSudoku);
+  MostrarTableroFinal(Sudoku, PistasSudoku);
   writeln('Presione cualquier tecla para salir.');
   ReadKey;
 end;
 BEGIN
   JugarSudoku;
 END.
+//Made by: GG & MC//
